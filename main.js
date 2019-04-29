81c4c9db48e24aaf0f89a304bed99172b309d586
ch264 <christinavhastenrath@gmail.com>
2019-04-29 11:11:19 -0700

    refractor code

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-
-window.addEventListener('keydown', function(e) {
+function playSound(e) {
 	let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 	let key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 	// stops function from running if no audio is found
@@ -7,20 +6,23 @@ window.addEventListener('keydown', function(e) {
 	// rewinds audio to start
 	audio.currentTime = 0;
 	audio.play();
-	// add class
+	// adds class on button down
 	key.classList.add('playing');
-	// remove class with transition end event
+}
 
-});
-
+// removes css class styling
 function removeTransition(e) {
-	// skips if not transform
+	// skips if not transform property
 	if (e.propertyName !== 'transform') return;
 	this.classList.remove('playing');
 }
+
 
 // grabs all keys
 const keys = document.querySelectorAll('.key')
 // loop over every key element and attach an event listener
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+// event listener 
+window.addEventListener('keydown', playSound);
 
