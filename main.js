291f0aff87395df1adb441b5fdabd156c2181102
ch264 <christinavhastenrath@gmail.com>
2019-04-29 10:46:09 -0700

    added div containers

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 
 window.addEventListener('keydown', function(e) {
 	let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-
-	console.log(audio)
+	let key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+	// stops function from running if no audio is found
+	if(!audio) return;
+	// rewinds audio to start
+	audio.currentTime = 0;
+	audio.play();
+	console.log(key)
 });
