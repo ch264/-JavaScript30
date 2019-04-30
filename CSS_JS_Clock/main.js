ad6747d4d6cc9ab852d7cd4c30778fec250e9ed9
ch264 <christinavhastenrath@gmail.com>
2019-04-30 15:49:56 -0700

    added styling and finished day 2

--- CSS_JS_Clock/main.js
+++ CSS_JS_Clock/main.js
@@ -1,6 +1,6 @@
 
 const secondHand = document.querySelector('.second')
-const minuteHand = document.querySelector('.minutes');
+const minuteHand = document.querySelector('.minute');
 const hourHand = document.querySelector('.hour')
 
 function setDate() {
@@ -9,13 +9,19 @@ function setDate() {
 	// gets seconds of current minute
 	const seconds = now.getSeconds();
 	// caclulates degree angle and offsets default 90 degrees
-	const secondsDegrees = (seconds / 60) * 360 + 90
-	secondHand.style.transform = `rotate(${secondsDegrees})`;
+	const secondsDegrees = ((seconds / 60) * 360) + 90
+	secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
 	const minutes = now.getMinutes();
-	const minDegrees = (minutes / 60) * 360 + 90 ;
-	minuteHand.style.transform = `rotate(${minDegrees})`;
+	const minDegrees = ((minutes / 60) * 360) + 90 ;
+	minuteHand.style.transform = `rotate(${minDegrees}deg)`;
+
+	const hour = now.getHours();
+	const hourDegrees = ((hour / 12) * 360) +((minutes / 60 ) * 30) + 90 ;
+	hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
 
 setInterval(setDate, 1000);
+
+setDate();
