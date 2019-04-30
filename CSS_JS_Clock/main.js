cbd26ebf376d429d6eb9582763019f02cfcfb3b7
ch264 <christinavhastenrath@gmail.com>
2019-04-30 15:31:46 -0700

    added minute function

--- CSS_JS_Clock/main.js
+++ CSS_JS_Clock/main.js
@@ -1,13 +1,21 @@
 
 const secondHand = document.querySelector('.second')
+const minuteHand = document.querySelector('.minutes');
+const hourHand = document.querySelector('.hour')
 
 function setDate() {
 	const now = new Date();
+
 	// gets seconds of current minute
 	const seconds = now.getSeconds();
 	// caclulates degree angle and offsets default 90 degrees
 	const secondsDegrees = (seconds / 60) * 360 + 90
 	secondHand.style.transform = `rotate(${secondsDegrees})`;
+
+	const minutes = now.getMinutes();
+	const minDegrees = (minutes / 60) * 360 + 90 ;
+	minuteHand.style.transform = `rotate(${minDegrees})`;
 }
+
 
 setInterval(setDate, 1000);
