9f7a3a4516ed5549745e078d2e89ba4228f11d38
ch264 <christinavhastenrath@gmail.com>
2019-06-21 18:18:48 -0700

    added functionality to rgb sliders. Completed Day 19

--- Webcam_fun/scripts.js
+++ Webcam_fun/scripts.js
@@ -8,14 +8,16 @@ const snap = document.querySelector('.snap');
 
 
 function getVideo() {
-	navigator.mediaDevices.getUserMedia({ video: true, audio: false}).then(localMediaStream => {
+	navigator.mediaDevices.getUserMedia({ video: true, audio: false})
+	.then(localMediaStream => {
 		// console.log(localMediaStream);
 		// convert object into URL
 		// video.src = window.URL.createObjectURL(localMediaStream);
 		video.srcObject = localMediaStream;
 		video.play();
-	}).catch(err => {
-		console.error('error here, turn webcam on', err)
+	})
+	.catch(err => {
+		console.error(`error here, turn webcam on`, err)
 	});
 }
 
@@ -27,7 +29,19 @@ function paintToCanvas() {
 
 	// take image from webcam and set to canvas, starting at top left hand cornerand paint width and height. return the interval so that you have access to it and you can stop it from paining.
 	return setInterval(() => {
-		ctx.drawImage(video, 0, 0, width, height)
+		ctx.drawImage(video, 0, 0, width, height);
+		// add filters and use let to reassign pixels when they are taken out of the canvas
+		let pixels = ctx.getImageData(0, 0, width, height)
+		
+		// //mess with pixels ///////////////////////////////
+		// pixels = redEffect(pixels);
+		// pixels = rgbSplit(pixels);
+		// ghosting effect
+		// ctx.globalAlpha = 0.1;
+		pixels = greenScreen(pixels);
+
+		// put pixels back
+		ctx.putImageData(pixels, 0, 0);
 	}, 16);
 }
 
@@ -43,6 +57,52 @@ function takePhoto() {
 	// link.textContent = 'Download Image';
 	link.innerHTML = `<img src="${data}" alt="handsome" />`
 	strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+	// pixels is not an array, therefore use data.length
+	for (let i = 0; i < pixels.data.length; i+=4) {
+		pixels.data[i + 0] = pixels.data[i+0] + 100;  // red
+		pixels.data[i + 1] = pixels.data[i+1] - 50; // green
+		pixels.data[i + 2] = pixels.data[i+2] * 0.5; // blue
+	}
+	return pixels;
+}
+
+function rgbSplit(pixels) {
+	for (let i = 0; i < pixels.data.length; i+=4) {
+		pixels.data[i - 150] = pixels.data[i+0] + 100;  // red
+		pixels.data[i + 100] = pixels.data[i+1] - 50; // green
+		pixels.data[i - 500] = pixels.data[i+2] * 0.5; // blue
+	}
+	return pixels;
+};
+
+function greenScreen(pixels) {
+	// holde minimum and maximum green. give me all the colors between this rgb value and take them out
+	const levels = {};
+
+	// grab every rgb input sliders
+	document.querySelectorAll('.rgb input').forEach((input) => { levels[input.name] = input.value;
+	});
+	// loops over every single pixel
+	for (i = 0; i < pixels.data.length; i = i + 4) {
+		red = pixels.data[i + 0];
+		green = pixels.data[i + 1];
+		blue = pixels.data[i + 2];
+		alpha = pixels.data[i + 3];
+
+		// if pixels are inbetween min and max values, take it out.
+		if (red >= levels.rmin
+			&& green >= levels.gmin
+			&& blue >= levels.bmin
+			&& red <= levels.rmax
+			&& green <= levels.gmax
+			&& blue <= levels.bmax) {
+				pixels.data[i + 3] = 0;
+			}
+	}
+	return pixels;
 }
 
 getVideo();
